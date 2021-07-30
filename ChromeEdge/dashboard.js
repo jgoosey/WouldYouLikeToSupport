03aab66d1363e1f046d9ccfe76e523333a8b4867
Jon Goss <2957948+jgoosey@users.noreply.github.com>
2021-07-30 16:57:10 -0400

    v1 extension (Chrome)

--- ChromeEdge/dashboard.js
+++ ChromeEdge/dashboard.js
@@ -6,11 +6,9 @@ function onError(e) {
 }
 
 function getURL() {
-  console.log("in function");
-  browser.tabs.query({currentWindow: true, active: true}, function(tabs){
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
     alert("Current URL: " + tabs[0].url);
   });
-  console.log("out function");
 }
 
 document.getElementById("btn").addEventListener("click", getURL);
