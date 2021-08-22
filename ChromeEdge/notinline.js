a2e66485dba817fe76f94bcf75df891794ce0cdd
Jon Goss <2957948+jgoosey@users.noreply.github.com>
2021-08-21 22:11:28 -0400

    updates. new table. still messing with css

---
+++ ChromeEdge/notinline.js
@@ -0,0 +1,20 @@
+// from: https://github.com/derekeder/csv-to-html-table/blob/master/index.html
+
+function format_link(link) {
+  if (link) return "<a href='" + link + "' target='_blank'>" + link + "</a>";
+  else return "";
+}
+
+CsvToHtmlTable.init({
+  csv_path: "data/codes.csv",
+  element: "table-container",
+  allow_download: false,
+  csv_options: {
+    separator: ",",
+    delimiter: '"',
+  },
+  datatables_options: {
+    paging: false,
+  },
+  custom_formatting: [[2, format_link]],
+});
