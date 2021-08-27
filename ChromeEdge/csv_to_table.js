39d2d5840582d10bfde512e3f762658162649cf4
Jon Goss <2957948+jgoosey@users.noreply.github.com>
2021-08-27 16:01:44 -0400

    unpacked v1.0
    changes to table, extension size, etc. updates to local source files

--- ChromeEdge/csv_to_table.js
+++ ChromeEdge/csv_to_table.js
@@ -1,3 +1,85 @@
+// from: https://datatables.net/plug-ins/dataRender/hyperLink
+
+jQuery.fn.dataTable.render.hyperLink = function (
+  anchorText,
+  location,
+  width,
+  height
+) {
+  validateAndReturnDefaultIfFailed = function (item, defaultValue) {
+    if (typeof item === "number") {
+      return item;
+    }
+
+    if (typeof item === "string") {
+      return parseInt(item) ? item : defaultValue;
+    }
+
+    return defaultValue;
+  };
+
+  var anchorText = anchorText || "Click here";
+  var location = location || "newTab";
+  var width = validateAndReturnDefaultIfFailed(width, "600");
+  var height = validateAndReturnDefaultIfFailed(height, "400");
+
+  return function (data, type, row) {
+    // restriction only for table display rendering
+    if (type !== "display") {
+      return data;
+    }
+
+    var url = data;
+
+    try {
+      url = new URL(data);
+
+      switch (location) {
+        case "newTab":
+          return (
+            '<a title="' +
+            url +
+            '" href="' +
+            url +
+            '" target="_blank">' +
+            anchorText +
+            "</a>"
+          );
+        case "popup":
+          return (
+            '<a title="' +
+            url +
+            '" href="' +
+            url +
+            '" target="popup" rel="noopener noreferrer" onclick="window.open(\'' +
+            url +
+            "', '" +
+            anchorText +
+            "', 'width=" +
+            width +
+            ",height=" +
+            height +
+            "'); return false;\">" +
+            anchorText +
+            "</a>"
+          );
+        default:
+          return (
+            '<a title="' +
+            url +
+            '" href="' +
+            url +
+            '" target="_blank">' +
+            anchorText +
+            "</a>"
+          );
+      }
+    } catch (e) {
+      return url;
+    }
+  };
+};
+
 // from: https://github.com/pavelsr/csv-to-DataTableer/blob/master/index.js
 
 function createTable(tableData, callback) {
@@ -28,14 +110,29 @@ function createTable(tableData, callback) {
 function DataTable() {
   $("#codes_table").DataTable({
     paging: true,
-    scrollY: 400,
+    scrollY: 363,
     scrollX: false,
     info: false,
-    language: {
-      searchPlaceholder: "Search by name, website, or code!",
+    responsive: true,
+    colReorder: {
+      order: [0, 1, 3, 2],
     },
+    language: {
+      searchPlaceholder: "name, website, or code",
+    },
+    columnDefs: [
+      {
+        targets: 2,
+        render: $.fn.dataTable.render.hyperLink(),
+      },
+      { orderable: false, targets: 2 },
+    ],
+    /* lengthMenu: [
+      [10, 25, 50, -1],
+      [10, 25, 50, "All"],
+    ], */
   });
-  console.log("DataTable finished");
+  //console.log("DataTable finished");
 }
 
 Papa.parse("data/codes.csv", {
@@ -45,10 +142,9 @@ Papa.parse("data/codes.csv", {
   delimiter: ",",
   error: true,
   fastMode: true,
-  // quoteChar: '"',
   complete: function (results) {
-    // console.log(results.meta.fields);
-    console.log("Parsing complete:", results.data);
+    //console.log(results.meta.fields);
+    //console.log("Parsing complete:", results.data);
     createTable(results, DataTable);
   },
 });
